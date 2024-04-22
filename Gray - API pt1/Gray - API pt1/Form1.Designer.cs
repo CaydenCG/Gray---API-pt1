9e865f3ffb2e688ca6b72f764510b54a5b8066bc
Cayden <Caydencgray@gmail.com>
2024-04-22 10:03:22 -0400

    Add project files.

---
+++ Gray - API pt1/Gray - API pt1/Form1.Designer.cs
@@ -0,0 +1,252 @@
+﻿namespace Gray___API_pt1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SearchButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.IDBox = new System.Windows.Forms.TextBox();
+            this.NameLabel = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.HappinessLabel = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.CaptureLabel = new System.Windows.Forms.Label();
+            this.HabitatLabel = new System.Windows.Forms.Label();
+            this.GrowthLabel = new System.Windows.Forms.Label();
+            this.FlavorLabel = new System.Windows.Forms.Label();
+            this.EggLabel = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // SearchButton
+            // 
+            this.SearchButton.Location = new System.Drawing.Point(168, 80);
+            this.SearchButton.Name = "SearchButton";
+            this.SearchButton.Size = new System.Drawing.Size(75, 23);
+            this.SearchButton.TabIndex = 0;
+            this.SearchButton.Text = "Search";
+            this.SearchButton.UseVisualStyleBackColor = true;
+            this.SearchButton.Click += new System.EventHandler(this.SearchButton_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(103, 124);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 16);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Name";
+            // 
+            // IDBox
+            // 
+            this.IDBox.Location = new System.Drawing.Point(180, 52);
+            this.IDBox.Name = "IDBox";
+            this.IDBox.Size = new System.Drawing.Size(50, 22);
+            this.IDBox.TabIndex = 3;
+            // 
+            // NameLabel
+            // 
+            this.NameLabel.AutoSize = true;
+            this.NameLabel.Location = new System.Drawing.Point(228, 124);
+            this.NameLabel.Name = "NameLabel";
+            this.NameLabel.Size = new System.Drawing.Size(79, 16);
+            this.NameLabel.TabIndex = 4;
+            this.NameLabel.Text = "placeholder";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(103, 152);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(108, 16);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Base Happiness";
+            // 
+            // HappinessLabel
+            // 
+            this.HappinessLabel.AutoSize = true;
+            this.HappinessLabel.Location = new System.Drawing.Point(228, 152);
+            this.HappinessLabel.Name = "HappinessLabel";
+            this.HappinessLabel.Size = new System.Drawing.Size(79, 16);
+            this.HappinessLabel.TabIndex = 6;
+            this.HappinessLabel.Text = "placeholder";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(103, 179);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(86, 16);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Capture Rate";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(103, 207);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(50, 16);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Habitat";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(103, 232);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(80, 16);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Growth Rate";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(357, 124);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(74, 16);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "Flavor Text";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(103, 261);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(72, 16);
+            this.label7.TabIndex = 11;
+            this.label7.Text = "Egg Group";
+            // 
+            // CaptureLabel
+            // 
+            this.CaptureLabel.AutoSize = true;
+            this.CaptureLabel.Location = new System.Drawing.Point(228, 179);
+            this.CaptureLabel.Name = "CaptureLabel";
+            this.CaptureLabel.Size = new System.Drawing.Size(79, 16);
+            this.CaptureLabel.TabIndex = 12;
+            this.CaptureLabel.Text = "placeholder";
+            // 
+            // HabitatLabel
+            // 
+            this.HabitatLabel.AutoSize = true;
+            this.HabitatLabel.Location = new System.Drawing.Point(228, 207);
+            this.HabitatLabel.Name = "HabitatLabel";
+            this.HabitatLabel.Size = new System.Drawing.Size(79, 16);
+            this.HabitatLabel.TabIndex = 13;
+            this.HabitatLabel.Text = "placeholder";
+            // 
+            // GrowthLabel
+            // 
+            this.GrowthLabel.AutoSize = true;
+            this.GrowthLabel.Location = new System.Drawing.Point(228, 232);
+            this.GrowthLabel.Name = "GrowthLabel";
+            this.GrowthLabel.Size = new System.Drawing.Size(79, 16);
+            this.GrowthLabel.TabIndex = 14;
+            this.GrowthLabel.Text = "placeholder";
+            // 
+            // FlavorLabel
+            // 
+            this.FlavorLabel.AutoSize = true;
+            this.FlavorLabel.Location = new System.Drawing.Point(482, 124);
+            this.FlavorLabel.Name = "FlavorLabel";
+            this.FlavorLabel.Size = new System.Drawing.Size(79, 16);
+            this.FlavorLabel.TabIndex = 15;
+            this.FlavorLabel.Text = "placeholder";
+            // 
+            // EggLabel
+            // 
+            this.EggLabel.AutoSize = true;
+            this.EggLabel.Location = new System.Drawing.Point(228, 261);
+            this.EggLabel.Name = "EggLabel";
+            this.EggLabel.Size = new System.Drawing.Size(79, 16);
+            this.EggLabel.TabIndex = 16;
+            this.EggLabel.Text = "placeholder";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(137, 29);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(143, 16);
+            this.label8.TabIndex = 17;
+            this.label8.Text = "Enter an ID from 1-1025";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(726, 297);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.EggLabel);
+            this.Controls.Add(this.FlavorLabel);
+            this.Controls.Add(this.GrowthLabel);
+            this.Controls.Add(this.HabitatLabel);
+            this.Controls.Add(this.CaptureLabel);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.HappinessLabel);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.NameLabel);
+            this.Controls.Add(this.IDBox);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.SearchButton);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button SearchButton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox IDBox;
+        private System.Windows.Forms.Label NameLabel;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label HappinessLabel;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label CaptureLabel;
+        private System.Windows.Forms.Label HabitatLabel;
+        private System.Windows.Forms.Label GrowthLabel;
+        private System.Windows.Forms.Label FlavorLabel;
+        private System.Windows.Forms.Label EggLabel;
+        private System.Windows.Forms.Label label8;
+    }
+}
+
---
+++ Gray - API pt1/Gray - API pt1/Form1.cs
@@ -0,0 +1,46 @@
+﻿using PokeAPI;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Gray___API_pt1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        public async void GetSpecies(int id)
+        {
+            PokemonSpecies p = await DataFetcher.GetApiObject<PokemonSpecies>(id);
+            NameLabel.Text = p.Name;
+            HappinessLabel.Text = p.BaseHappiness.ToString();
+            CaptureLabel.Text = p.CaptureRate.ToString();
+            HabitatLabel.Text = p.Habitat.Name;
+            GrowthLabel.Text = p.GrowthRate.Name;
+            FlavorLabel.Text = p.FlavorTexts[0].FlavorText;
+            EggLabel.Text = string.Join(", ", p.EggGroups.Select(eggGroup => eggGroup.Name));
+        }
+
+        private void SearchButton_Click(object sender, EventArgs e)
+        {
+            if (int.TryParse(IDBox.Text, out int id) && id >= 1 && id <= 1025)
+            {
+                GetSpecies(id);
+            }
+            else
+            {
+                MessageBox.Show("Please enter a number between 1 and 1025.");
+            }
+        }
+    }
+}
